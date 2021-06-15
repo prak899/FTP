66e63812b8d4d6f04997f9a53202fad231551a7e
SS <majhi9442@gmail.com>
2021-06-15 16:08:41 +0530

    Initial commit

---
+++ app/src/main/java/in/pm/ftp/Spalsh.java
@@ -0,0 +1,37 @@
+package in.pm.ftp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
+
+import in.pm.ftp.FTP.ImageUploadActivity;
+
+public class Spalsh extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Spalsh.this, ImageUploadActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
